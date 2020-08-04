02fee7413e4ff8af3e305ab18d32db71efa63c8f
dpeddi <dpeddi@gmail.com>
2020-08-04 23:57:23 +0200

    lot of improvements including saving of configurations on spiffs and reduced flickering

--- ArduinoWeatherStation/ArduinoWeatherStation.ino
+++ ArduinoWeatherStation/ArduinoWeatherStation.ino
@@ -1,3 +1,4 @@
+#include <FS.h>          // this needs to be first, or it all crashes and burns..
 #include <SPI.h>
 #include <Wire.h>
 
@@ -11,6 +12,7 @@
 #include <ArduinoJson.h>
 #include "WeatherData.h"
 #include <ArduinoOTA.h>
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 
 // Color definitions
 #define BLACK    0x0000
@@ -27,26 +29,161 @@
 #define cs   D8
 #define dc   D3
 #define rst  D4
+//#define dc   D4
+//#define rst  -1
+
+// select which pin will trigger the configuration portal when set to LOW
+#define TRIGGER_PIN 0
+
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
+//define your default values here, if there are different values in config.json, they are overwritten.
+char api_user[20] = ""; //add your own geonames username
+char api_key[33] = ""; //add your own accuweather API KEY
+char city[40] = "Frankfurt "; // add your prefered city
+char units [40]= "metric"; // (options: metric/imperial )
+
+int timeout = 120; // seconds to run for
+WiFiManager wm; // global wm instance
+// setup custom parameters
+// 
+// The extra parameters to be configured (can be either global or just in the setup)
+// After connecting, parameter.getValue() will get you the configured value
+// id/name placeholder/prompt default length
+WiFiManagerParameter custom_api_user("api_user", "api user", api_user, 20);
+WiFiManagerParameter custom_api_key("api_key", "api key", api_key, 32);
+WiFiManagerParameter custom_city("city", "city", city, 40);
+WiFiManagerParameter custom_units("units", "units", units, 40);
+
+bool portalRunning      = false;
 
 WeatherData weatherData = WeatherData();
-
-const String API_KEY = ""; //add your own accuweather API KEY
-String city = "Frankfurt "; // add your prefered city
-String units = "metric"; // (options: metric/imperial )
-
-const char* ssid = ""; //TODO REMOVE before push to github
-const char* password = ""; //TODO REMOVE before push to github
 
 // Instantiate the TFT constructor with the pin values defined above
 Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst);
 
 long weatherDataTimer = 0;
 
+float coord_lon = NULL; 
+float coord_lat = NULL;
+
+char hostString[16] = {0};
+
+void checkButton(){
+  // check for button press
+  if ( digitalRead(TRIGGER_PIN) == LOW && (!portalRunning)) {
+    // poor mans debounce/press-hold, code not ideal for production
+    delay(50);
+    if( digitalRead(TRIGGER_PIN) == LOW ){
+      Serial.println("Button Pressed");
+      // still holding button for 3000 ms, reset settings, code not ideaa for production
+      delay(3000); // reset delay hold
+      if( digitalRead(TRIGGER_PIN) == LOW ){
+        Serial.println("Button Held");
+        Serial.println("Erasing Config, restarting");
+        wm.resetSettings();
+        ESP.restart();
+      }
+      
+      // start portal w delay
+      Serial.println("Starting config portal");
+      wm.setConfigPortalTimeout(timeout);
+      
+      if (!wm.startConfigPortal(String(hostString).c_str())) {
+        Serial.println("failed to connect or hit timeout");
+        delay(3000);
+        // ESP.restart();
+      } else {
+        //if you get here you have connected to the WiFi
+        Serial.println("connected...yeey :)");
+      }
+    } 
+  } else if (!portalRunning) {
+      Serial.println("Button Pressed, Starting Web Portal");
+      wm.startWebPortal();
+  }
+  portalRunning = true;
+  
+  if (portalRunning){
+    wm.process();
+  }
+}
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+void setupSpiffs(){
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(api_user, json["api_user"]);
+          strcpy(api_key, json["api_key"]);
+          strcpy(city, json["city"]);
+          strcpy(units, json["units"]);
+
+          // if(json["ip"]) {
+          //   Serial.println("setting custom ip from config");
+          //   strcpy(static_ip, json["ip"]);
+          //   strcpy(static_gw, json["gateway"]);
+          //   strcpy(static_sn, json["subnet"]);
+          //   Serial.println(static_ip);
+          // } else {
+          //   Serial.println("no custom ip in config");
+          // }
+
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+}
+
 void setup () {
 
   Serial.begin(115200);
   Serial.println("setup()");
+  pinMode(TRIGGER_PIN, INPUT_PULLUP);
+
   delay(500);
+
+  sprintf(hostString, "ESPWEATHER_%06X", ESP.getChipId());
+  Serial.println(hostString);
+
+  setupSpiffs();
+  
+  //set config save notify callback
+  wm.setSaveConfigCallback(saveConfigCallback);
+
+  //add all your parameters here
+  wm.addParameter(&custom_api_user);
+  wm.addParameter(&custom_api_key);
+  wm.addParameter(&custom_city);
+  wm.addParameter(&custom_units);
 
   Wire.begin();
 
@@ -57,35 +194,72 @@ void setup () {
   tft.setTextSize(1);
   tft.print("Connecting...");
 
-  char hostString[16] = {0};
-  sprintf(hostString, "ESPWEATHER_%06X", ESP.getChipId());
-  Serial.println(hostString);
   WiFi.hostname(hostString);
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print("Connecting..\n");
+  
+  if(wm.autoConnect(hostString)){
+      Serial.println("connected...yeey :)");
+      tft.fillScreen(BLACK);
+      tft.setCursor(30, 80);
+      tft.setTextColor(WHITE);
+      tft.setTextSize(1);
+      //tft.print("Connected");
+  } else {
+      Serial.println("Configportal running");
+      tft.fillScreen(BLACK);
+      tft.setCursor(30, 80);
+      tft.setTextColor(WHITE);
+      tft.setTextSize(1);
+      tft.print("connect to " + String(hostString) + "to configure");
   }
   
   ArduinoOTA.setHostname(hostString);
   ArduinoOTA.begin();
 
-  tft.fillScreen(BLACK);
-  tft.setCursor(30, 80);
-  tft.setTextColor(WHITE);
-  tft.setTextSize(1);
-  tft.print("Connected");
+
 
   initWeather();
 }
 
 void loop() {
   ArduinoOTA.handle();
+  checkButton();
+  
   if (millis() - weatherDataTimer > 60000) {
     initWeather();
     weatherDataTimer = millis();
+  }
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    //read updated parameters
+    strcpy(api_user, custom_api_user.getValue());
+    strcpy(api_key, custom_api_key.getValue());
+    strcpy(city, custom_city.getValue());
+    strcpy(units, custom_units.getValue());
+
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["api_user"]   = api_user;
+    json["api_key"]   = api_key;
+    json["city"] = city;
+    json["units"]   = units;
+
+    // json["ip"]          = WiFi.localIP().toString();
+    // json["gateway"]     = WiFi.gatewayIP().toString();
+    // json["subnet"]      = WiFi.subnetMask().toString();
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+
+    json.prettyPrintTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+    shouldSaveConfig = false;
   }
 }
 
@@ -98,18 +272,23 @@ void initWeather() {
     tft.print("Disconnected");
   } else {
     getWeatherData();
+	if (coord_lat != NULL && coord_lon != NULL)
+       getCurrentTimeRequest(coord_lat, coord_lon);
+
   }
 }
 
 
 void getWeatherData() {
-  String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&APPID=" + API_KEY;
+  String url = "http://api.openweathermap.org/data/2.5/weather?q=" + String(city) + "&units=" + String(units) + "&APPID=" + String(api_key);
 
   HTTPClient http;  //Declare an object of class HTTPClient
   http.begin(url);  //Specify request destination
   int httpCode = http.GET();//Send the request
-  if (httpCode > 0) { //Check t he returning code
+  Serial.println("\ngetWeatherData: "+ String(httpCode) + " "+ url);
+  if (httpCode == 200) { //Check t he returning code
     String payload = http.getString();   //Get the request response payload
+	Serial.println(payload);
     //parse data
     parseWeatherData(payload);
   }
@@ -126,8 +305,8 @@ void parseWeatherData(String payload) {
 
   JsonObject& root = jsonBuffer.parseObject(payload);
 
-  float coord_lon = root["coord"]["lon"]; // 25.61
-  float coord_lat = root["coord"]["lat"]; // 45.65
+  coord_lon = root["coord"]["lon"]; // 25.61
+  coord_lat = root["coord"]["lat"]; // 45.65
 
   JsonObject& weather_0 = root["weather"][0];
   int weather_0_id = weather_0["id"]; // 803
@@ -163,14 +342,15 @@ void parseWeatherData(String payload) {
   const char* cityName = root["name"]; // CITY
   int cod = root["cod"]; // 200
 
-  tft.fillScreen(BLACK);
-
-  getCurrentTimeRequest(coord_lat, coord_lon);
+  //tft.fillScreen(BLACK);
+  tft.fillRect(0, 0 , tft.width(), 58, BLACK);
 
   displayTemperature(main_temp);
   displayIcon(weather_0_icon);
   displayDescription(weather_0_description);
-  displaySunriseTime(sys_sunrise);
+  
+  tft.fillRect(0, 105 , tft.width(), tft.height() - 105, BLACK);
+  displaySunriseTime(sys_sunrise);  
   displaySunsetTime(sys_sunset);
   displayLocation(cityName);
 
@@ -178,16 +358,19 @@ void parseWeatherData(String payload) {
 }
 
 void getCurrentTimeRequest(float latitude, float longitude) {
-  String url = "http://api.geonames.org/timezoneJSON?lat=" + String(latitude) + "&lng=" + String(longitude) + "&username=kode";
+  String url = "http://api.geonames.org/timezoneJSON?lat=" + String(latitude) + "&lng=" + String(longitude) + "&username=" + String(api_user);
 
   HTTPClient http;  //Declare an object of class HTTPClient
   http.begin(url);  //Specify request destination
   int httpCode = http.GET();//Send the request
-  if (httpCode > 0) { //Check the returning code
+  Serial.println("\ngetCurrentTimeRequest: "+ String(httpCode) + " "+ url);
+  if (httpCode == 200) { //Check the returning code
     String payload = http.getString();   //Get the request response payload
+	Serial.println(payload);
     //parse data
     parseTimeData(payload);
   }
+  Serial.println("getCurrentTimeRequest: "+String(httpCode) );
   http.end();   //Close connection
 }
 
@@ -209,13 +392,14 @@ void parseTimeData(String payload) {
   const char* currentTime = root["time"]; // "2019-03-08 01:11"
   float lat = root["lat"]; // 45.65
 
-  Serial.print(currentTime);
+  Serial.println(currentTime);
 
   displayCurrentTime(currentTime);
 }
 
 // TIME
 void displayCurrentTime(String currentTime) {
+  tft.fillRect(0, 70, tft.width(), 30, BLACK);
   tft.setTextSize(3);
   tft.setCursor(1, 70);
 
